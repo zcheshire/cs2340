a05b5255560b668a4e03f8182f21801893bcbbfc
John Bonaguro <jbonaguro2@gmail.com>
2017-02-14 16:26:21 -0500

    Created transition from main to login activity

--- Water/app/src/main/java/com/gitrekt/water/MainActivity.java
+++ Water/app/src/main/java/com/gitrekt/water/MainActivity.java
@@ -1,7 +1,15 @@
 package com.gitrekt.water;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
