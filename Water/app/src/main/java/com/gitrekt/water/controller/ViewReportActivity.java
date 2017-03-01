714ec61865a145601bad4fd526c8332e1ad81d3d
Zachary Cheshire <zcheshire3@gatech.edu>
2017-03-01 16:12:56 -0500

    Added ViewReport activity

---
+++ Water/app/src/main/java/com/gitrekt/water/controller/ViewReportActivity.java
@@ -0,0 +1,15 @@
+package com.gitrekt.water.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.gitrekt.water.R;
+
+public class ViewReportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_report);
+    }
+}
