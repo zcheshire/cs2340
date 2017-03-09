192d90bfb659147761e8a2e8c46489e3182c3c8c
Ray, Jerod <jray35@gatech.edu>
2017-03-09 16:44:20 -0500

    ListView Implementation

---
+++ Water/app/src/main/java/com/gitrekt/water/controller/WaterReportAdapter.java
@@ -0,0 +1,64 @@
+package com.gitrekt.water.controller;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.content.*;
+import com.gitrekt.water.R;
+import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+
+import com.gitrekt.water.model.UserReport;
+
+import java.util.ArrayList;
+
+/**
+ * Created by jerodray on 3/7/17.
+ */
+
+public class WaterReportAdapter extends BaseAdapter {
+
+    private Context myContext;
+    private ArrayList<UserReport> userReports;
+
+    public WaterReportAdapter(Context myContext, ArrayList<UserReport> userReports) {
+        this.myContext = myContext;
+        this.userReports = userReports;
+    }
+
+    @Override
+    public int getCount() {
+        return userReports.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return userReports.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View v = View.inflate(myContext, R.layout.activity_view_report, null);
+        TextView tvUser = (TextView) v.findViewById(R.id.user);
+        TextView tvType = (TextView) v.findViewById(R.id.type);
+        TextView tvCondition = (TextView) v.findViewById(R.id.condition);
+        TextView tvLocation = (TextView) v.findViewById(R.id.location);
+        TextView tvPostID = (TextView) v.findViewById(R.id.postID);
+
+        tvUser.setText(userReports.get(position).getUser().getUserName());
+        tvType.setText(userReports.get(position).getType().toString());
+        tvCondition.setText(userReports.get(position).getCondition().toString());
+        tvLocation.setText(userReports.get(position).getLocation());
+        tvPostID.setText(userReports.get(position).getReportNumber());
+
+        //v.setTag(userReports.get(position).getReportNumber());
+
+        return v;
+
+    }
+}
